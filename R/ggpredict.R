d32641682c7b10b7b3016f92906035873d96acbf
Daniel <mail@danielluedecke.de>
2017-07-03 19:03:36 +0200

    remove globalVariables

--- R/ggpredict.R
+++ R/ggpredict.R
@@ -1,5 +1,3 @@
-utils::globalVariables(c("observed", "predicted"))
-
 #' @title Get marginal effects from model terms
 #' @name ggpredict
 #'
@@ -269,7 +267,7 @@ ggpredict_helper <- function(model, terms, ci.lvl, type, full.data, typical, ...
   if (full.data) {
     mydf <- dplyr::mutate(mydf,
       observed = sjmisc::to_value(fitfram[[1]], start.at = 0, keep.labels = F),
-      residuals = observed - predicted
+      residuals = .data$observed - .data$predicted
     )
   } else {
     mydf <- dplyr::mutate(mydf, observed = NA, residuals = NA)
