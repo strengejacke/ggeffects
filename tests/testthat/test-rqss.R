8d8ab52f47735b2a27f09074db7f1636c5fe1a06
Daniel <mail@danielluedecke.de>
2019-04-01 12:02:17 +0200

    fix test

--- tests/testthat/test-rqss.R
+++ tests/testthat/test-rqss.R
@@ -16,6 +16,9 @@ if (suppressWarnings(
   m1 <- rqss(y ~ qss(x,constraint="I") + z, data = dat)
 
   test_that("ggpredict, rq", {
+
+    skip_on_travis()
+
     pr <- ggpredict(m1, "x [1:13 by=.5]")
     expect_equal(pr$predicted[1], 1.304986, tolerance = 1e-4)
   })
