c85b5b1d0ace908005537b6fd3698d446e005774
Daniel <mail@danielluedecke.de>
2019-05-13 11:34:54 +0200

    no time-consuming tests on CRAN

--- tests/testthat/test-brms-trial.R
+++ tests/testthat/test-brms-trial.R
@@ -1,24 +1,28 @@
-if (suppressWarnings(
-  require("testthat") &&
-  require("brms") &&
-  require("ggeffects")
-)) {
-  context("ggeffects, brms-trial")
+.runThisTest <- Sys.getenv("RunAllggeffectsTests") == "yes"
 
-  m1 <- insight::download_model("brms_mixed_6")
-  m2 <- insight::download_model("brms_mv_4")
-  m3 <- insight::download_model("brms_2")
+if (.runThisTest) {
+  if (suppressWarnings(
+    require("testthat") &&
+    require("brms") &&
+    require("ggeffects")
+  )) {
+    context("ggeffects, brms-trial")
 
-  test_that("ggpredict, brms-trial", {
-    ggpredict(m1, c("Base", "Trt"))
-    ggpredict(m2, "Species")
-    ggpredict(m3, c("treat", "c2"))
-  })
+    m1 <- insight::download_model("brms_mixed_6")
+    m2 <- insight::download_model("brms_mv_4")
+    m3 <- insight::download_model("brms_2")
 
-  test_that("ggpredict, brms-trial", {
-    p1 <- ggpredict(m1, c("Base", "Trt"))
-    p2 <- ggemmeans(m1, c("Base", "Trt"))
-    expect_equal(p1$predicted[1], p2$predicted[1], tolerance = 1e-3)
-  })
+    test_that("ggpredict, brms-trial", {
+      ggpredict(m1, c("Base", "Trt"))
+      ggpredict(m2, "Species")
+      ggpredict(m3, c("treat", "c2"))
+    })
 
+    test_that("ggpredict, brms-trial", {
+      p1 <- ggpredict(m1, c("Base", "Trt"))
+      p2 <- ggemmeans(m1, c("Base", "Trt"))
+      expect_equal(p1$predicted[1], p2$predicted[1], tolerance = 1e-3)
+    })
+
+  }
 }
