a215da4240169454aa5c1d05465014a986bd3693
Daniel <mail@danielluedecke.de>
2019-06-08 09:50:54 +0200

    add msg

--- R/utils_ggpredict.R
+++ R/utils_ggpredict.R
@@ -5,24 +5,27 @@
 
   rv <- insight::find_variables(model)[["response"]]
 
-  if (back.transform && any(grepl("log\\((.*)\\)", rv))) {
-
-    # do we have log-log models?
-    if (grepl("log\\(log\\((.*)\\)\\)", rv)) {
-      mydf$predicted <- exp(exp(mydf$predicted))
-      if (obj_has_name(mydf, "conf.low") && obj_has_name(mydf, "conf.high")) {
-        mydf$conf.low <- exp(exp(mydf$conf.low))
-        mydf$conf.high <- exp(exp(mydf$conf.high))
+  if (any(grepl("log\\((.*)\\)", rv))) {
+    if (back.transform) {
+      # do we have log-log models?
+      if (grepl("log\\(log\\((.*)\\)\\)", rv)) {
+        mydf$predicted <- exp(exp(mydf$predicted))
+        if (obj_has_name(mydf, "conf.low") && obj_has_name(mydf, "conf.high")) {
+          mydf$conf.low <- exp(exp(mydf$conf.low))
+          mydf$conf.high <- exp(exp(mydf$conf.high))
+        }
+      } else {
+        mydf$predicted <- exp(mydf$predicted)
+        if (obj_has_name(mydf, "conf.low") && obj_has_name(mydf, "conf.high")) {
+          mydf$conf.low <- exp(mydf$conf.low)
+          mydf$conf.high <- exp(mydf$conf.high)
+        }
       }
+
+      message("Model has log-transformed response. Back-transforming predictions to original response scale. Standard errors are still on the log-scale.")
     } else {
-      mydf$predicted <- exp(mydf$predicted)
-      if (obj_has_name(mydf, "conf.low") && obj_has_name(mydf, "conf.high")) {
-        mydf$conf.low <- exp(mydf$conf.low)
-        mydf$conf.high <- exp(mydf$conf.high)
-      }
+      message("Model has log-transformed response. Predictions are on log-scale.")
     }
-
-    message("Model has log-transformed response. Back-transforming predictions to original response scale. Standard errors are still on the log-scale.")
   }
 
   mydf
