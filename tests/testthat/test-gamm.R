9ed33ca6988d6293b6da20098e3c47def01ca137
Daniel Lüdecke <mail@danielluedecke.de>
2019-09-02 10:03:15 +0200

    fix test

--- tests/testthat/test-gamm.R
+++ tests/testthat/test-gamm.R
@@ -14,7 +14,7 @@ if (require("testthat") && require("ggeffects") && require("mgcv")) {
 
   test_that("ggpredict", {
     p <- ggpredict(m1, "x1")
-    expect_equal(p$predicted[1], 2.743739, tolerance = 1e-3)
+    expect_equal(p$predicted[1], 15.5450060160087, tolerance = 1e-3)
     ggpredict(m1, c("x1", "x2"))
   })
 
