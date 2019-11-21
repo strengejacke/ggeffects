d1e19cb02f70e7c2b104d2f3fb806eb845edb2e0
Daniel <mail@danielluedecke.de>
2019-11-21 10:45:32 +0100

    ok, add comment twice ;-)

--- R/utils_get_data_grid.R
+++ R/utils_get_data_grid.R
@@ -385,7 +385,11 @@
   # save constant values as attribute
   attr(datlist, "constant.values") <- constant_values
   attr(datlist, "n.trials") <- n.trials
-  if(length(terms) > 1) {
+
+  # remember if grouping "factor" is numeric. this is possibly required
+  # later when plotting data points for continuous predictors that are
+  # held constant at their mean/sd values or similar.
+  if (length(terms) > 1) {
     attr(datlist, "continuous.group") <- is.numeric(datlist[[2]])
   } else {
     attr(datlist, "continuous.group") <- FALSE
