73c849d400c4dd38c8bd41dae8a836ddd78c083f
Daniel <mail@danielluedecke.de>
2020-10-27 16:53:24 +0100

    work on predict() for parameters::pool_parameters()

---
+++ WIP/own_predict_method.R
@@ -0,0 +1,31 @@
+m <- lm(Sepal.Length ~ Species + Sepal.Width, data = iris)
+
+nd <- ggeffects::new_data(m, "Species", condition = c(Sepal.Width = 50))
+
+
+m <- lm(Sepal.Length ~ Species + Sepal.Width, data = iris)
+Terms <- delete.response(terms(m))
+mf <- model.frame(Terms, data = nd)
+X <- model.matrix(Terms, mf)
+
+# predictions
+beta <- coef(m)
+result <- as.numeric(crossprod(beta, t(X)))
+
+# SE
+sqrt(diag(X %*% vcov(m) %*% t(X)))
+
+
+ggeffects:::.safe_se_from_vcov(
+  m,
+  nd,
+  value_adjustment = "mean",
+  vcov.fun = NULL,
+  terms = "Species",
+  condition = c(Sepal.Width = 50),
+  type = "fe",
+  vcov.args = NULL,
+  vcov.type = NULL,
+  interval = NULL,
+  model_class = "lm"
+)
