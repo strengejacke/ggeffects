992204006864995ddc3109977b75bac8b69c1151
Daniel <mail@danielluedecke.de>
2020-11-07 21:13:09 +0100

    no empty test

--- tests/testthat/test-betareg.R
+++ tests/testthat/test-betareg.R
@@ -5,18 +5,18 @@ if (require("testthat") && require("ggeffects") && require("betareg")) {
   test_that("ggpredict", {
     p <- ggpredict(m1, "batch")
     expect_equal(p$predicted, unname(predict(m1, newdata = new_data(m1, "batch"))), tolerance = 1e-3)
-    ggpredict(m1, c("batch", "temp"))
+    expect_is(ggpredict(m1, c("batch", "temp")), "data.frame")
   })
 
   test_that("ggeffect", {
     p <- ggeffect(m1, "batch")
     expect_equal(p$predicted[1], 0.3122091, tolerance = 1e-3)
-    ggeffect(m1, c("batch", "temp"))
+    expect_is(ggeffect(m1, c("batch", "temp")), "data.frame")
   })
 
   test_that("ggemmeans", {
     p <- ggemmeans(m1, "batch")
     expect_equal(p$predicted[1], 0.3122091, tolerance = 1e-3)
-    ggemmeans(m1, c("batch", "temp"))
+    expect_is(ggemmeans(m1, c("batch", "temp")), "data.frame")
   })
 }
