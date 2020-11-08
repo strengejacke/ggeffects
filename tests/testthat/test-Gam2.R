3c7245d34afabfaf7e4a55b0ab0c0e03da6b9918
Daniel <mail@danielluedecke.de>
2020-11-08 10:57:29 +0100

    ...

--- tests/testthat/test-Gam2.R
+++ tests/testthat/test-Gam2.R
@@ -5,24 +5,24 @@ if (require("testthat") && require("ggeffects") && require("gam")) {
     Kyphosis ~ s(Age, 4) + Number,
     family = binomial,
     data = kyphosis,
-    trace = TRUE
+    trace = FALSE
   )
 
   test_that("ggpredict", {
     p <- ggpredict(m1, "Age")
     expect_equal(p$predicted[1], 0.02043849, tolerance = 1e-3)
-    ggpredict(m1, c("Age", "Number"))
+    expect_is(ggpredict(m1, c("Age", "Number")), "data.frame")
   })
 
   test_that("ggeffect", {
     p <- ggeffect(m1, "Age")
     expect_equal(p$predicted[1], 0.106151, tolerance = 1e-3)
-    ggeffect(m1, c("Age", "Number"))
+    expect_is(ggeffect(m1, c("Age", "Number")), "data.frame")
   })
 
   test_that("ggemmeans", {
     p <- ggemmeans(m1, "Age")
     expect_equal(p$predicted[1], 0.02043849, tolerance = 1e-3)
-    ggemmeans(m1, c("Age", "Number"))
+    expect_is(ggemmeans(m1, c("Age", "Number")), "data.frame")
   })
 }
