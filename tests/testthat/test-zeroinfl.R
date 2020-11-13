52856dd6c609dc269edb7f6b55bba1a293ce8d8d
Daniel <mail@danielluedecke.de>
2020-11-13 09:07:33 +0100

    add test

--- tests/testthat/test-zeroinfl.R
+++ tests/testthat/test-zeroinfl.R
@@ -81,4 +81,35 @@ if (suppressWarnings(
     expect_equal(p1$predicted[1], p2$predicted[1], tolerance = 1e-3)
   })
 
+  #Generate some data
+
+  set.seed(123)
+  N <- 100 #Samples
+  x <- runif(N, 0, 5) #Predictor 1
+  z <- runif(N, 0, 5) #Predictor 2
+  off <- rgamma(N, 3, 2) #Offset variable
+  yhat <- -1 + x * 0.2 + z * -0.2 + z * x * 0.2 + log(off) #Prediction on log scale
+  dat <- data.frame(y = NA, x,z, logOff = log(off)) #Storage dataframe
+
+  dat$y <- rpois(N, exp(yhat)) #Poisson process
+  dat$y <- ifelse(rbinom(N, 1, 0.3), 0, dat$y) #Zero-inflation process
+
+  #Fit zeroinfl and glm model
+
+  #Interaction b/w x and z
+  model <- zeroinfl(y ~ offset(logOff) + x * z | 1, data = dat, dist = 'poisson')
+
+  test_that("pscl, offset, interaction and CI", {
+    pr <- ggpredict(model, c("x", "z"))
+    expect_equal(
+      pr$conf.low,
+      c(0.09963, 0.11356, 0.10043, 0.15167, 0.18376, 0.17829, 0.2295,
+        0.29636, 0.31618, 0.34394, 0.47536, 0.55973, 0.50699, 0.75547,
+        0.98802, 0.72668, 1.18187, 1.73484, 0.99808, 1.80482, 3.016,
+        1.30413, 2.6781, 5.15324, 1.63447, 3.87834, 8.6113, 1.99384,
+        5.52767, 14.12902, 2.39388, 7.80484, 22.93352),
+      tolerance = 1e-3
+    )
+  })
+
 }
