31fb8482263d6c0791364d049b5a0d7b1313178c
Daniel <mail@danielluedecke.de>
2021-03-26 10:54:54 +0100

    Update test-coxph.R

--- tests/testthat/test-coxph.R
+++ tests/testthat/test-coxph.R
@@ -23,4 +23,43 @@ if (require("testthat") && require("ggeffects") && require("emmeans") && require
     p <- ggpredict(m1, "sex", type = "cumhaz")
     expect_equal(p$predicted[1], 0.003325958, tolerance = 1e-2)
   })
+
+  # test start of time
+  df1 <- data.frame(
+    production = c(15, 12, 10, 9, 6, 8, 9, 5, 3, 3, 2, 1, 0, 0, 0, 0),
+    Treatment_Num = c(1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4),
+    Genotype = c(1, 1, 2, 2, 1, 1, 2, 2, 1, 1, 2, 2, 1, 1, 2, 2),
+    Source_Salinity = c("Fresh", "Fresh", "Brackish", "Brackish", "Fresh", "Fresh",
+                        "Brackish", "Brackish", "Fresh", "Fresh", "Brackish", "Brackish",
+                        "Fresh", "Fresh", "Brackish", "Brackish"),
+    Days_to_death = c(500, 500, 500, 500, 400, 350, 300, 500, 200, 202, 260,
+                      280, 150, 150, 160, 140),
+    censored = c(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0)
+  )
+  m <- coxph(Surv(time = Days_to_death, event = censored) ~ Treatment_Num + Source_Salinity, data = df1)
+
+  test_that("ggpredict", {
+    p <- ggpredict(m, c("Treatment_Num [all]", "Source_Salinity [all]"),type = "survival")
+    expect_equal(p$x, c(1, 1, 1, 1, 1, 1, 1, 1, 140, 140, 140, 140, 140, 140, 140,
+                        140, 150, 150, 150, 150, 150, 150, 150, 150, 160, 160, 160, 160,
+                        160, 160, 160, 160, 200, 200, 200, 200, 200, 200, 200, 200, 202,
+                        202, 202, 202, 202, 202, 202, 202, 260, 260, 260, 260, 260, 260,
+                        260, 260, 280, 280, 280, 280, 280, 280, 280, 280, 300, 300, 300,
+                        300, 300, 300, 300, 300, 350, 350, 350, 350, 350, 350, 350, 350,
+                        400, 400, 400, 400, 400, 400, 400, 400, 500, 500, 500, 500, 500,
+                        500, 500), tolerance = 1e-2)
+    expect_equal(head(p$predicted, 10), c(1, 1, 1, 1, 1, 1, 1, 1, 1, 1), tolerance = 1e-2)
+
+    p <- ggpredict(m, c("Treatment_Num [all]", "Source_Salinity [all]"),type = "cumulative_hazard")
+    expect_equal(p$x, c(1, 1, 1, 1, 1, 1, 1, 1, 140, 140, 140, 140, 140, 140, 140,
+                        140, 150, 150, 150, 150, 150, 150, 150, 150, 160, 160, 160, 160,
+                        160, 160, 160, 160, 200, 200, 200, 200, 200, 200, 200, 200, 202,
+                        202, 202, 202, 202, 202, 202, 202, 260, 260, 260, 260, 260, 260,
+                        260, 260, 280, 280, 280, 280, 280, 280, 280, 280, 300, 300, 300,
+                        300, 300, 300, 300, 300, 350, 350, 350, 350, 350, 350, 350, 350,
+                        400, 400, 400, 400, 400, 400, 400, 400, 500, 500, 500, 500, 500,
+                        500, 500), tolerance = 1e-2)
+    expect_equal(head(p$predicted, 10), c(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), tolerance = 1e-2)
+  })
+
 }
