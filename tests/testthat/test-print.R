e929c4c5ce835f711f95fabc3724e5ef51c3bbf5
Daniel <mail@danielluedecke.de>
2021-04-10 16:20:01 +0200

    Update test-print.R

--- tests/testthat/test-print.R
+++ tests/testthat/test-print.R
@@ -1,6 +1,8 @@
 if (suppressWarnings(
   require("testthat") &&
   require("ggeffects") &&
+  require("haven") &&
+  require("sjlabelled") &&
   require("sjmisc")
 )) {
   # lm, linear regression ----
