71eef4006ea3f387e01a601146d2976383bd2aeb
Daniel <mail@danielluedecke.de>
2021-04-11 11:45:04 +0200

    Update test-gamlss.R

--- tests/testthat/test-gamlss.R
+++ tests/testthat/test-gamlss.R
@@ -14,7 +14,7 @@ if (.runThisTest && require("testthat") && require("ggeffects") && require("gaml
                    4.97144, 5.13182, 5.2922, 5.45257, 5.61295, 5.77333),
                  tolerance = 1e-2)
     expect_equal(colnames(p), c("x", "predicted", "std.error", "conf.low", "conf.high", "group"))
-    expect_equal(p$predicted,
+    expect_equal(p$conf.low,
                  c(3.66712, 3.85232, 4.03678, 4.22004, 4.40113, 4.5782, 4.74796,
                    4.90714, 5.05622, 5.19881, 5.33789, 5.47507, 5.61117),
                  tolerance = 1e-2)
