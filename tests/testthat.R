a27cd15d0b2bfa3ba1bf09b7aa1b3c514b028c6f
Daniel <mail@danielluedecke.de>
2021-07-28 23:41:47 +0200

    fix CRAN check issues

--- tests/testthat.R
+++ tests/testthat.R
@@ -7,5 +7,37 @@ if (require("testthat")) {
     Sys.setenv("RunAllggeffectsTests" = "no")
   }
 
-  test_check("ggeffects")
+
+  si <- Sys.info()
+
+  osx <- tryCatch(
+    {
+      if (!is.null(si["sysname"])) {
+        si["sysname"] == "Darwin" || grepl("^darwin", R.version$os)
+      } else {
+        FALSE
+      }
+    },
+    error = function(e) {
+      FALSE
+    }
+  )
+
+
+  solaris <- tryCatch(
+    {
+      if (!is.null(si["sysname"])) {
+        grepl("SunOS", si["sysname"], ignore.case = TRUE)
+      } else {
+        FALSE
+      }
+    },
+    error = function(e) {
+      FALSE
+    }
+  )
+
+  if (!osx && !solaris) {
+    test_check("ggeffects")
+  }
 }
