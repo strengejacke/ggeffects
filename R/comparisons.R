89ff969f470d15495f31087747063ea0555460b6
Daniel <mail@danielluedecke.de>
2023-02-16 21:29:17 +0100

    allow slopes

--- R/comparisons.R
+++ R/comparisons.R
@@ -18,37 +18,77 @@
 comparisons <- function(x, test = "pairwise") {
   insight::check_if_installed("marginaleffects")
 
-  # retrieve relevant information and generate data grid for predictions
+  # retrieve focal predictors
   focal <- attributes(x)$terms
+  # retrieve relevant information and generate data grid for predictions
   model <- .get_model_object(x)
   grid <- insight::get_datagrid(model, focal)
-
   # grid <- expand.grid(c(attributes(x)$at.list, attributes(x)$constant.values))
 
-  .comparisons <- marginaleffects::predictions(
-    model,
-    newdata = grid,
-    hypothesis = test
-  )
+  # comparisons only make sense if we have at least two predictors, or if
+  # we have one categorical
+  focal_numeric <- vapply(grid[focal], is.numeric, TRUE)
+  focal_other <- !focal_numeric
 
-  if (any(startsWith(.comparisons$term, "Row"))) {
-    contrast_terms <- data.frame(
-      do.call(rbind, strsplit(.comparisons$term, " - ", fixed = TRUE)),
-      stringsAsFactors = FALSE
+  if (length(focal) > 1 && all(focal_numeric)) {
+    insight::format_error(
+      "Pairwise comparisons for multiple focal predictors can only be computed if at least one predictor is categorical."
     )
-    contrast_terms[] <- lapply(contrast_terms, function(i) {
-      insight::trim_ws(gsub("Row", "", i, fixed = TRUE))
-    })
+  }
 
-    out <- as.data.frame(lapply(focal, function(i) {
-      unlist(lapply(seq_len(nrow(contrast_terms)), function(j) {
-        .contrasts <- grid[[i]][as.numeric(unlist(contrast_terms[j, ]))]
-        .contrasts_string <- paste(.contrasts, collapse = "-")
-      }))
-    }))
-    names(out) <- focal
+  # reorder grid, making numeric first
+  if (any(focal_numeric)) {
+    grid[c(which(focal_numeric), which(focal_other))] <- grid[focal]
+    colnames(grid)[c(which(focal_numeric), which(focal_other))] <- focal
+    # reorder focal terms to match grid
+    focal <- focal[c(which(focal_numeric), which(focal_other))]
+  }
+
+  if (any(focal_numeric)) {
+    if (length(focal) == 1) {
+      .comparisons <- marginaleffects::avg_slopes(model, variables = focal)
+      out <- data.frame(x_ = "slope", stringsAsFactors = FALSE)
+    } else {
+      .comparisons <- marginaleffects::slopes(
+        model,
+        variables = focal[1],
+        by = focal[2:length(focal)],
+        hypothesis = test
+      )
+
+      if (!is.null(test) && all(test == "pairwise")) {
+      } else {
+        grid_categorical <- unique(as.data.frame(grid[focal[2:length(focal)]]))
+        out <- cbind(data.frame(x_ = "slope", stringsAsFactors = FALSE), grid_categorical)
+      }
+    }
+    colnames(out) <- focal
   } else {
-    out <- data.frame(Comparison = .comparisons$term, stringsAsFactors = FALSE)
+    .comparisons <- marginaleffects::predictions(
+      model,
+      newdata = grid,
+      hypothesis = test
+    )
+
+    if (!is.null(test) && all(test == "pairwise")) {
+      contrast_terms <- data.frame(
+        do.call(rbind, strsplit(.comparisons$term, " - ", fixed = TRUE)),
+        stringsAsFactors = FALSE
+      )
+      contrast_terms[] <- lapply(contrast_terms, function(i) {
+        insight::trim_ws(gsub("Row", "", i, fixed = TRUE))
+      })
+
+      out <- as.data.frame(lapply(focal, function(i) {
+        unlist(lapply(seq_len(nrow(contrast_terms)), function(j) {
+          .contrasts <- grid[[i]][as.numeric(unlist(contrast_terms[j, ]))]
+          .contrasts_string <- paste(.contrasts, collapse = "-")
+        }))
+      }))
+      colnames(out) <- focal
+    } else {
+      out <- data.frame(Comparison = .comparisons$term, stringsAsFactors = FALSE)
+    }
   }
 
   # further results
