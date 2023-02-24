a89fb17a50b4431731003f0d5f205f268b4c68e5
Daniel <mail@danielluedecke.de>
2023-02-24 09:18:11 +0100

    fix

--- R/utils_get_cleaned_terms.R
+++ R/utils_get_cleaned_terms.R
@@ -18,3 +18,13 @@
   # be sure to remove any brackets
   trimws(sub("[", "", x, fixed = TRUE))
 }
+
+
+.list_to_character_terms <- function(x) {
+  if (is.list(x)) {
+    x <- unlist(lapply(names(x), function(i) {
+      paste0(i, " [", toString(x[[i]]), "]")
+    }))
+  }
+  x
+}
