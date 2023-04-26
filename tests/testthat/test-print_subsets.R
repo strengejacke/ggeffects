0d0e1c84e6461a03418b9f9149d7d9d5526e1f21
Daniel <mail@danielluedecke.de>
2023-04-26 22:11:18 +0200

    skip snapshot on linux

--- tests/testthat/test-print_subsets.R
+++ tests/testthat/test-print_subsets.R
@@ -1,3 +1,5 @@
+skip_on_os(c("mac", "linux"))
+
 if (suppressWarnings(requiet("testthat") && requiet("ggeffects"))) {
   test_that("print subsetting 1", {
     m <- lm(mpg ~ gear * factor(cyl), mtcars)
