8c79ba66b926ba106e17834dd969a00bff33841a
Daniel <mail@danielluedecke.de>
2023-05-12 09:46:46 +0200

    fix test

--- tests/testthat/test-interval_re.R
+++ tests/testthat/test-interval_re.R
@@ -18,11 +18,11 @@ if (.runThisTest && suppressWarnings(
     expect_message(print(out1), regex = "prediction")
     expect_no_message(print(out3))
     expect_message(
-      print(hypothesis_test(ggpredict(m, "Species", type = "random"))),
+      print(hypothesis_test(out1)),
       regex = "Intervals"
     )
     expect_no_message(
-      print(hypothesis_test(ggpredict(m, "Species", type = "random"), verbose = FALSE))
+      print(hypothesis_test(out1, verbose = FALSE))
     )
   })
 
