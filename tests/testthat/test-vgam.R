ff7be45e6376687a8537f09e5912ad3193df39ea
Daniel <mail@danielluedecke.de>
2023-10-29 17:22:43 +0100

    fix

--- tests/testthat/test-vgam.R
+++ tests/testthat/test-vgam.R
@@ -5,5 +5,5 @@ test_that("ggpredict", {
   data("hunua", package = "VGAM")
   m1 <- VGAM::vgam(agaaus ~ vitluc + VGAM::s(altitude, df = 2), VGAM::binomialff, data = hunua)
   p <- ggpredict(m1, "vitluc")
-  expect_equal(p$predicted[1], 0.2745789, tolerance = 1e-3)
+  expect_equal(p$predicted[1], 0.2491865, tolerance = 1e-3)
 })
