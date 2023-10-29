7159093d30bfaab41ee7076efc6121b042e98a2e
Daniel <mail@danielluedecke.de>
2023-10-29 16:42:58 +0100

    fix

--- tests/testthat/test-vglm.R
+++ tests/testthat/test-vglm.R
@@ -1,64 +1,61 @@
+skip_on_cran()
+skip_if_not_installed("VGAM")
 unloadNamespace("gam")
 
-skip_on_cran()
-
-if (requiet("testthat") && requiet("ggeffects") && requiet("VGAM")) {
+test_that("ggpredict", {
   d.AD <- data.frame(
     treatment = gl(3, 3),
     outcome = gl(3, 1, 9),
     counts = c(18, 17, 15, 20, 10, 20, 25, 13, 12)
   )
+  m1 <- VGAM::vglm(
+    counts ~ outcome + treatment,
+    family = VGAM::poissonff,
+    data = d.AD,
+    trace = TRUE
+  )
+  p <- ggpredict(m1, "outcome")
+  expect_equal(p$predicted[1], 21, tolerance = 1e-3)
+})
 
-  m1 <- vglm(counts ~ outcome + treatment, family = poissonff, data = d.AD, trace = TRUE)
 
-  test_that("ggpredict", {
-    p <- ggpredict(m1, "outcome")
-    expect_equal(p$predicted[1], 21, tolerance = 1e-3)
-  })
-
-
-
-
+test_that("ggpredict", {
   set.seed(123)
-  N     <- 100
-  X1    <- rnorm(N, 175, 7)
-  X2    <- rnorm(N,  30, 8)
+  N <- 100
+  X1 <- rnorm(N, 175, 7)
+  X2 <- rnorm(N, 30, 8)
   Ycont <- 0.5 * X1 - 0.3 * X2 + 10 + rnorm(N, 0, 6)
 
-  Yord  <- cut(
+  Yord <- cut(
     Ycont,
     breaks = quantile(Ycont),
     include.lowest = TRUE,
     labels = c("--", "-", "+", "++"),
     ordered = TRUE
   )
-
   dfOrd <- data.frame(X1, X2, Yord)
+  m2 <- VGAM::vglm(Yord ~ X1 + X2, family = VGAM::propodds, data = dfOrd)
+  p <- ggpredict(m2, terms = "X1")
 
-  m2 <- vglm(Yord ~ X1 + X2, family = propodds, data = dfOrd)
+  expect_equal(p$predicted[1], 0.2633227, tolerance = 1e-3)
+  expect_identical(nrow(p), 27L)
 
-  test_that("ggpredict", {
-    p <- ggpredict(m2, terms = "X1")
-    expect_equal(p$predicted[1], 0.2633227, tolerance = 1e-3)
-    expect_identical(nrow(p), 27L)
-
-    p <- ggpredict(m2, terms = "X1", ci_level = NA)
-    expect_equal(p$predicted[1], 0.7366773, tolerance = 1e-3)
-    expect_identical(nrow(p), 36L)
-  })
+  p <- ggpredict(m2, terms = "X1", ci_level = NA)
+  expect_equal(p$predicted[1], 0.7366773, tolerance = 1e-3)
+  expect_identical(nrow(p), 36L)
+})
 
 
-  data(pneumo)
+test_that("ggpredict", {
+  data(pneumo, package = "VGAM")
   pneumo <- transform(pneumo, let = log(exposure.time))
-  m3 <- vglm(cbind(normal, mild, severe) ~ let, propodds, data = pneumo)
+  m3 <- VGAM::vglm(cbind(normal, mild, severe) ~ let, VGAM::propodds, data = pneumo)
 
-  test_that("ggpredict", {
-    p <- ggpredict(m3, "let")
-    expect_equal(p$predicted[1], 0.005992263, tolerance = 1e-3)
-    expect_identical(nrow(p), 16L)
+  p <- ggpredict(m3, "let")
+  expect_equal(p$predicted[1], 0.005992263, tolerance = 1e-3)
+  expect_identical(nrow(p), 16L)
 
-    p <- ggpredict(m3, "let", ci_level = NA)
-    expect_equal(p$predicted[1], 0.9940077, tolerance = 1e-3)
-    expect_identical(nrow(p), 24L)
-  })
-}
+  p <- ggpredict(m3, "let", ci_level = NA)
+  expect_equal(p$predicted[1], 0.9940077, tolerance = 1e-3)
+  expect_identical(nrow(p), 24L)
+})
