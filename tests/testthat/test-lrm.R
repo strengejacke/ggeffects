609566a4094b2fe513ff23f49f9b3fae7dff3d70
Daniel <mail@danielluedecke.de>
2023-11-02 21:54:27 +0100

    use withr

--- tests/testthat/test-lrm.R
+++ tests/testthat/test-lrm.R
@@ -4,18 +4,6 @@ skip_if_not_installed("effects")
 skip_if_not_installed("emmeans")
 skip_if_not_installed("withr")
 
-# test_that("ggpredict, lrm", {
-#   data(efc, package = "ggeffects")
-#   efc$neg_c_7d <- as.numeric(efc$neg_c_7 > median(efc$neg_c_7, na.rm = TRUE))
-#   d <<- efc
-#   m1 <- rms::lrm(neg_c_7d ~ c12hour + e42dep + c161sex + c172code, data = d)
-#   pr <- ggpredict(m1, "c12hour")
-#   expect_equal(pr$predicted[1], 0.4008948, tolerance = 1e-2)
-#   expect_null(ggeffect(m1, "c12hour"))
-#   pr <- ggemmeans(m1, "c12hour")
-#   expect_equal(pr$predicted[1], 0.4008948, tolerance = 1e-2)
-# })
-
 withr::with_environment(
   new.env(),
   test_that("ggpredict, lrm", {
