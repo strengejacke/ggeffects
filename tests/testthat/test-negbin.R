db6d43c35135587661880d3dae02da5b6b1a8daa
Daniel <mail@danielluedecke.de>
2023-11-02 14:30:34 +0100

    fix

--- tests/testthat/test-negbin.R
+++ tests/testthat/test-negbin.R
@@ -1,44 +1,35 @@
-if (suppressWarnings(
-  requiet("testthat") &&
-  requiet("ggeffects") &&
-  requiet("sjmisc") &&
-  requiet("MASS")
-)) {
+skip_if_not_installed("MASS")
+
+test_that("ggpredict, negbin", {
   data(efc, package = "ggeffects")
   efc$e42dep <- to_label(efc$e42dep)
-  fit <-
-    glm.nb(
-      tot_sc_e ~ neg_c_7 + I(neg_c_7 ^ 2) + neg_c_7:e42dep + I(neg_c_7 ^ 2):e42dep + c12hour + c172code,
-      data = efc,
-      init.theta = 1.133641349,
-      link = log
-    )
+  fit <- MASS::glm.nb(
+    tot_sc_e ~ neg_c_7 + I(neg_c_7^2) + neg_c_7:e42dep + I(neg_c_7^2):e42dep + c12hour + c172code,
+    data = efc,
+    init.theta = 1.133641349,
+    link = log
+  )
+  expect_s3_class(ggpredict(fit, "neg_c_7"), "data.frame")
+  expect_s3_class(ggeffect(fit, "neg_c_7"), "data.frame")
+  # still fails on windows old-rel, so re-activate once emmeans is built on all platforms
+  expect_null(ggemmeans(fit, "neg_c_7"))
+  expect_s3_class(ggpredict(fit, c("neg_c_7", "e42dep")), "data.frame")
+  expect_s3_class(ggeffect(fit, c("neg_c_7", "e42dep")), "data.frame")
+  expect_null(ggemmeans(fit, c("neg_c_7", "e42dep")))
+})
 
-  test_that("ggpredict, negbin", {
-    expect_s3_class(ggpredict(fit, "neg_c_7"), "data.frame")
-    expect_s3_class(ggeffect(fit, "neg_c_7"), "data.frame")
-    # still fails on windows old-rel, so re-activate once emmeans is built on all platforms
-    # expect_null(ggemmeans(fit, "neg_c_7"))
-    expect_s3_class(ggpredict(fit, c("neg_c_7", "e42dep")), "data.frame")
-    expect_s3_class(ggeffect(fit, c("neg_c_7", "e42dep")), "data.frame")
-    # expect_null(ggemmeans(fit, c("neg_c_7", "e42dep")))
-  })
-
+test_that("ggpredict, negbin", {
   data(efc, package = "ggeffects")
-  fit <-
-    glm.nb(
-      tot_sc_e ~ neg_c_7 + I(neg_c_7 ^ 2) + neg_c_7:e42dep + I(neg_c_7 ^ 2):e42dep + c12hour + c172code,
-      data = efc,
-      init.theta = 1.133641349,
-      link = log
-    )
-
-  test_that("ggpredict, negbin", {
-    expect_s3_class(ggpredict(fit, "neg_c_7"), "data.frame")
-    expect_s3_class(ggeffect(fit, "neg_c_7"), "data.frame")
-    # expect_null(ggemmeans(fit, "neg_c_7"))
-    expect_s3_class(ggpredict(fit, c("neg_c_7", "e42dep")), "data.frame")
-    expect_s3_class(ggeffect(fit, c("neg_c_7", "e42dep")), "data.frame")
-    # expect_null(ggemmeans(fit, c("neg_c_7", "e42dep")))
-  })
-}
+  fit <- MASS::glm.nb(
+    tot_sc_e ~ neg_c_7 + I(neg_c_7^2) + neg_c_7:e42dep + I(neg_c_7^2):e42dep + c12hour + c172code,
+    data = efc,
+    init.theta = 1.133641349,
+    link = log
+  )
+  expect_s3_class(ggpredict(fit, "neg_c_7"), "data.frame")
+  expect_s3_class(ggeffect(fit, "neg_c_7"), "data.frame")
+  expect_null(ggemmeans(fit, "neg_c_7"))
+  expect_s3_class(ggpredict(fit, c("neg_c_7", "e42dep")), "data.frame")
+  expect_s3_class(ggeffect(fit, c("neg_c_7", "e42dep")), "data.frame")
+  expect_null(ggemmeans(fit, c("neg_c_7", "e42dep")))
+})
