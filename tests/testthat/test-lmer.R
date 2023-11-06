9a7d045de23c17e84f9502f00dcbd43ae8fa2f6a
Daniel <mail@danielluedecke.de>
2023-11-06 11:02:21 +0100

    tests

--- tests/testthat/test-lmer.R
+++ tests/testthat/test-lmer.R
@@ -10,12 +10,14 @@ data(efc, package = "ggeffects")
 efc$grp <- datawizard::to_factor(efc$e15relat)
 fit <- lme4::lmer(neg_c_7 ~ c12hour + e42dep + c161sex + c172code + (1 | grp), data = efc)
 
+
 test_that("validate ggpredict lmer against predict", {
   nd <- data_grid(fit, "e42dep")
   pr <- predict(fit, newdata = nd, re.form = NA)
   predicted <- ggpredict(fit, "e42dep")
   expect_equal(predicted$predicted, pr, tolerance = 1e-3, ignore_attr = TRUE)
 })
+
 
 test_that("validate ggpredict lmer against marginaleffects", {
   out1 <- marginaleffects::predictions(
@@ -45,6 +47,7 @@ test_that("validate ggpredict lmer against marginaleffects", {
   )
 })
 
+
 test_that("ggpredict, lmer", {
   expect_s3_class(ggpredict(fit, "c12hour"), "data.frame")
   expect_s3_class(ggpredict(fit, c("c12hour", "c161sex")), "data.frame")
@@ -54,18 +57,38 @@ test_that("ggpredict, lmer", {
   expect_s3_class(ggpredict(fit, c("c12hour", "c161sex", "c172code"), type = "re"), "data.frame")
 })
 
+
 test_that("ggpredict, lmer", {
   pr <- ggpredict(fit, "c12hour")
   expect_equal(pr$std.error[1:5], c(0.2911, 0.2852, 0.2799, 0.2752, 0.2713), tolerance = 1e-3)
   pr <- ggpredict(fit, c("c12hour", "c161sex", "c172code"), type = "re")
   expect_equal(pr$std.error[1:5], c(3.5882, 3.58185, 3.58652, 3.58162, 3.57608), tolerance = 1e-3)
+  # validate against predict
+  pr <- ggpredict(fit, "c12hour")
+  nd <- data_grid(fit, "c12hour")
+  pr2 <- suppressWarnings(predict(
+    fit,
+    newdata = nd,
+    se.fit = TRUE,
+    re.form = NA,
+    allow.new.levels = TRUE
+  ))
+  expect_equal(pr$std.error[1:5], pr2$se.fit[1:5], tolerance = 1e-3, ignore_attr = TRUE)
+  expect_equal(
+    pr$conf.low,
+    pr2$fit - qt(0.975, ggeffects:::.get_df(fit)) * pr2$se.fit,
+    tolerance = 1e-3,
+    ignore_attr = TRUE
+  )
 })
+
 
 test_that("ggpredict, lmer-simulate", {
   expect_s3_class(ggpredict(fit, "c12hour", type = "sim"), "data.frame")
   expect_s3_class(ggpredict(fit, c("c12hour", "c161sex"), type = "sim"), "data.frame")
   expect_s3_class(ggpredict(fit, c("c12hour", "c161sex", "c172code"), type = "sim"), "data.frame")
 })
+
 
 test_that("ggeffect, lmer", {
   expect_s3_class(ggeffect(fit, "c12hour"), "data.frame")
