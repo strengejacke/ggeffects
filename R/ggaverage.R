3086dc1e0f21894f9aa52496774df011feb0bd75
Daniel <mail@danielluedecke.de>
2023-11-07 11:53:50 +0100

    fix

--- R/ggaverage.R
+++ R/ggaverage.R
@@ -71,7 +71,7 @@ ggaverage <- function(model,
 
   # add attributes, needed for plotting etc.
   attributes(prediction_data) <- utils::modifyList(attributes(data_grid), attributes(prediction_data))
-  attr(predicted_data, "std.error") <- prediction_data$std.error
+  attr(prediction_data, "std.error") <- prediction_data$std.error
 
   result <- .post_processing_predictions(
     model = model,
