f04e8cbbf41523c2d96c4fee5a7dbf6ee586f58b
Daniel <mail@danielluedecke.de>
2023-11-09 11:08:58 +0100

    add tests for print-method

---
+++ tests/testthat/test-print_digits.R
@@ -0,0 +1,19 @@
+# skip_on_cran()
+skip_on_os(c("mac", "solaris"))
+
+test_that("ggpredict, print digits", {
+  data(iris)
+  m <- lm(Sepal.Length ~ Petal.Length, data = iris)
+  expect_snapshot(print(ggpredict(m, "Petal.Length"), digits = 5))
+  expect_snapshot(print(ggpredict(m, "Petal.Length"), digits = 4, n = 3))
+})
+
+test_that("ggpredict, print digits and labels", {
+  data(efc, package = "ggeffects")
+  efc$e42dep <- efc$e42dep
+  fit <- lm(barthtot ~ c12hour + e42dep, data = efc)
+
+  expect_snapshot(print(ggpredict(fit, "e42dep"), use_labels = FALSE))
+  expect_snapshot(print(ggpredict(fit, "e42dep"), use_labels = TRUE))
+  expect_snapshot(print(ggpredict(fit, "e42dep"), use_labels = TRUE, digits = 4))
+})
