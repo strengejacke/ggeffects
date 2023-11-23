87d2594bb27d8f5cff45a673a0c302265be08ca0
Daniel <mail@danielluedecke.de>
2023-11-23 16:27:17 +0100

    update test

--- tests/testthat/test-parsnip.R
+++ tests/testthat/test-parsnip.R
@@ -91,5 +91,6 @@ test_that("ggemmeans, parsnip", {
 
 test_that("test_predictions, parsnip", {
   out <- test_predictions(fit, "c172code")
-  expect_snapshot(print(out))
+  expect_equal(out$Slope, 0.71836, tolerance = 1e-3)
+  expect_equal(out$conf.low, -1.92486, tolerance = 1e-3)
 })
