d3a1a309d7134ddd4952feec9e54553ceedc86d5
Daniel <mail@danielluedecke.de>
2023-12-12 18:12:42 +0100

    add test

---
+++ tests/testthat/test-pool_comparisons.R
@@ -0,0 +1,16 @@
+skip_on_cran()
+skip_if_not_installed("mice")
+skip_if_not_installed("marginaleffects")
+
+test_that("ggeffects, pool comparisons", {
+  data("nhanes2", package = "mice")
+  set.seed(123)
+  imp <- mice::mice(nhanes2, printFlag = FALSE)
+  comparisons <- lapply(1:5, function(i) {
+    m <- lm(bmi ~ age + hyp + chl, data = mice::complete(imp, action = i))
+    hypothesis_test(m, "age")
+  })
+  pool_com <- pool_comparisons(comparisons)
+  expect_equal(pool_com$Contrast, c(4.6464, 6.70639, 2.05999), tolerance = 1e-3)
+  expect_equal(pool_come$conf.low, c(-0.07377, -0.81807, -4.02598), tolerance = 1e-3)
+})
