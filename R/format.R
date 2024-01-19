0ff6cef43ac73966dfe324814ac5714eff7bf28d
Daniel <mail@danielluedecke.de>
2024-01-19 19:57:39 +0100

    draft

--- R/format.R
+++ R/format.R
@@ -1,11 +1,26 @@
+#' @rdname print
 #' @export
 format.ggeffects <- function(x,
                              variable_labels = FALSE,
                              value_labels = FALSE,
                              group_name = FALSE,
                              row_header_separator = ", ",
+                             n,
                              ...) {
   insight::check_if_installed("datawizard")
+
+  # we need to determine how many rows to print. this requires the original
+  # data frame including attributes, that's why this code comes first
+  nrow_to_print <- .nrows_to_print(x, n)
+  focal_terms <- attributes(x)$terms
+
+  # fix terms for survival models
+  a1 <- attr(x, "fitfun", exact = TRUE)
+  a2 <- attr(x, "y.title", exact = TRUE)
+
+  if (!is.null(a1) && !is.null(a2) && a1 == "coxph" && a2 != "Risk Score" && !"time" %in% focal_terms) {
+    focal_terms <- c("time", focal_terms)
+  }
 
   # use value labels as values for focal term
   if (isTRUE(value_labels)) {
@@ -24,22 +39,26 @@ format.ggeffects <- function(x,
     x_label <- attributes(x)$x.title
     predicted_label <- attributes(x)$title
   } else {
-    x_label <- attributes(x)$terms[1]
+    x_label <- focal_terms[1]
     predicted_label <- "Predicted"
   }
 
+  # check if we have groups, facets, panels or response levels
   has_groups <- .obj_has_name(x, "group") && length(unique(x$group)) > 1
   has_facets <- .obj_has_name(x, "facet") && length(unique(x$facet)) > 1
   has_panel <- .obj_has_name(x, "panel") && length(unique(x$panel)) > 1
   has_response <- .obj_has_name(x, "response.level") && length(unique(x$response.level)) > 1
 
+  # check which columns we have - we want to sort by "subgroups"
   sort_columns <- c("response.level", "group", "facet", "panel")[c(has_response, has_groups, has_facets, has_panel)]
   x <- datawizard::data_arrange(x, sort_columns)
 
+  # format column names, to make it work with insight-formatting-functions
   x$CI <- 0.95
   colnames(x)[colnames(x) == "conf.low"] <- "CI_low"
   colnames(x)[colnames(x) == "conf.high"] <- "CI_high"
 
+  # format data frame
   x$x <- insight::format_value(x$x, protect_integers = TRUE, ...)
   x <- insight::format_table(x, zap_small = TRUE, ci_brackets = c("(", ")"), ...)
 
@@ -48,10 +67,19 @@ format.ggeffects <- function(x,
   # add variable name to group levels?
   if (isTRUE(group_name)) {
     for (i in sort_columns) {
-      x[[i]] <- paste(i, x[[i]], sep = ": ")
+      prefix <- switch(i,
+        response.level = "Response level",
+        group = ifelse(length(focal_terms) > 1, focal_terms[2], ""),
+        facet = ifelse(length(focal_terms) > 2, focal_terms[3], ""),
+        panel = ifelse(length(focal_terms) > 3, focal_terms[4], ""),
+        ""
+      )
+      prefix <- format(prefix, justify = "right", width = max(nchar(focal_terms)))
+      x[[i]] <- paste(prefix, x[[i]], sep = ": ")
     }
   }
 
+  # create labels, based on values from the different sort-columns ("subgroups")
   row_header_labels <- apply(x[sort_columns], 1, paste, collapse = row_header_separator)
   x[sort_columns] <- NULL
 
@@ -59,10 +87,67 @@ format.ggeffects <- function(x,
   colnames(x)[2] <- predicted_label
 
   x$groups <- row_header_labels
-  x
+
+  # split by groups, apply row selection (filtering), and combine data frame
+  tmp <- lapply(split(x, x$group), function(i) {
+    i[.get_sample_rows(i, n = nrow_to_print), , drop = FALSE]
+  })
+  # create data frame w/o rownames
+  out <- as.data.frame(do.call(rbind, tmp))
+  rownames(out) <- NULL
+  out
 }
 
 
+.nrows_to_print <- function(x, n = 10) {
+  # if we have groups, show n rows per group
+  .n <- 1
+
+  # remember if we have a factor
+  x_is_factor <- identical(attr(x, "x.is.factor"), "1") && is.factor(x$x)
+
+  # we do not simply count rows, but rather the number of combinations
+  # when we have facets / groups / response.levels. These are separated
+  # with own heading, making the output probably too long. Thus, we
+  # decide on how many rows per "subheading" to be printed also based on
+  # the number of combinations of groups, facets and response level
+  has_groups <- .obj_has_name(x, "group") && length(unique(x$group)) > 1
+  has_facets <- .obj_has_name(x, "facet") && length(unique(x$facet)) > 1
+  has_panel <- .obj_has_name(x, "panel") && length(unique(x$panel)) > 1
+  has_response <- .obj_has_name(x, "response.level") && length(unique(x$response.level)) > 1
+
+  # here comes the top-secret formula that calculates the perfect number of rows
+  if (has_groups) {
+    .n <- .n_distinct(x$group)
+  }
+  if (has_facets) {
+    .n <- .n * .n_distinct(x$facet)
+  }
+  if (has_panel) {
+    .n <- .n * .n_distinct(x$panel)
+  }
+  if (has_response) {
+    .n <- .n * .n_distinct(x$response.level)
+  }
+
+  # make sure that by default not too many rows are printed. The larger ".n" is
+  # (i.e. the more subheadings we have, see code above), the fewer rows we want
+  # per subheading. For factors, however, we want to show all levels
+  if (missing(n) && !x_is_factor) {
+    n <- if (.n >= 6) {
+      4
+    } else if (.n >= 4 && .n < 6) {
+      5
+    } else if (.n >= 2 && .n < 4) {
+      6
+    } else {
+      8
+    }
+  } else if (x_is_factor) {
+    n <- Inf
+  }
+  n
+}
 
 
 # xx <- format(x, row_header_separator = "\n# ", group_name = TRUE)
--- R/print.R
+++ R/print.R
@@ -1,190 +1,92 @@
+#' @title Print and format ggeffects-objects
+#' @name print
+#'
+#' @description A generic plot-method for `ggeffects`-objects.
+#'
+#' @param x An object of class `ggeffects`, as returned by the functions
+#'   from this package.
+#' @param ... Further arguments passed down to `ggplot::scale_y*()`, to
+#'    control the appearance of the y-axis.
+#'
+#' @return A ggplot2-object.
+#'
+#' @examples
+#' data(efc)
+#' efc$c172code <- as_label(efc$c172code)
+#' fit <- lm(barthtot ~ c12hour + neg_c_7 + c161sex + c172code, data = efc)
+#'
+#' dat <- ggpredict(fit, terms = "c12hour")
+#' plot(dat)
+#'
+#' \donttest{
+#' # facet by group, use pre-defined color palette
+#' dat <- ggpredict(fit, terms = c("c12hour", "c172code"))
+#' plot(dat, facet = TRUE, colors = "hero")
+#'
+#' # don't use facets, b/w figure, w/o confidence bands
+#' dat <- ggpredict(fit, terms = c("c12hour", "c172code"))
+#' plot(dat, colors = "bw", show_ci = FALSE)
+#'
+#' # factor at x axis, plot exact data points and error bars
+#' dat <- ggpredict(fit, terms = c("c172code", "c161sex"))
+#' plot(dat)
+#'
+#' # for three variables, automatic facetting
+#' dat <- ggpredict(fit, terms = c("c12hour", "c172code", "c161sex"))
+#' plot(dat)
+#' }
+#'
+#' # show all color palettes
+#' show_pals()
 #' @export
-print.ggeffects <- function(x, n = 10, digits = 2, value_labels = FALSE, verbose = TRUE, ...) {
-
-  # remember if we have a factor
-  x_is_factor <- identical(attr(x, "x.is.factor"), "1") && is.factor(x$x)
-
-  # use value labels as values in print
-  if (isTRUE(value_labels)) {
-    labs <- get_x_labels(x, case = NULL)
-    vals <- x$x
-
-    if (!is.null(labs)) {
-      x$x <- format(labs, justify = "right")
-      labs <- format(sprintf("[%g]", vals), justify = "left")
-      x$x <- paste(labs, x$x, sep = " ")
-    }
-  }
-
-  # remove std.error for printint
-  x$std.error <- NULL
-
-
-  # do we have groups and facets?
-  has_groups <- .obj_has_name(x, "group") && length(unique(x$group)) > 1
-  has_facets <- .obj_has_name(x, "facet") && length(unique(x$facet)) > 1
-  has_panel <- .obj_has_name(x, "panel") && length(unique(x$panel)) > 1
-  has_response <- .obj_has_name(x, "response.level") && length(unique(x$response.level)) > 1
-  has_se <- .obj_has_name(x, "std.error")
-
+print.ggeffects <- function(x, group_name = TRUE, verbose = TRUE, ...) {
   lab <- attr(x, "title", exact = TRUE)
-  if (!is.null(lab)) insight::print_color(paste0(sprintf("# %s", lab), "\n", collapse = ""), "blue")
+  if (!is.null(lab)) {
+    insight::print_color(paste0(sprintf("# %s", lab), "\n\n", collapse = ""), "blue")
+  }
 
-  # lab <- attr(x, "x.title", exact = TRUE)
-  # if (!is.null(lab)) insight::print_color(paste0(sprintf("# x = %s", lab), "\n", collapse = ""), "blue")
+  out <- format(x, row_header_separator = "\n", group_name = group_name, ...)
+  print_rows <- nrow(out)
+  captions <- unique(out$groups)
+  out <- split(out, out$groups)
 
+  out <- lapply(out, function(i) {
+    i$groups <- NULL
+    i
+  })
+
+  cat(insight::export_table(
+    out,
+    title = lapply(as.list(captions), c, "red"),
+    footer = .print_footnote(x)
+  ))
+  cat("\n")
+
+  # show msg?
+  if (missing(verbose)) {
+    verbose <- isTRUE(attr(x, "verbose", exact = TRUE))
+  }
+
+  predint <- attr(x, "prediction.interval", exact = TRUE)
+  if (!is.null(predint) && isTRUE(predint) && isTRUE(verbose)) {
+    insight::format_alert(
+      "\nIntervals are prediction intervals. Use `interval = \"confidence\"` to return regular confidence intervals."
+    )
+  }
+
+  # tell user about truncated output
+  if (print_rows < nrow(x) && isTRUE(verbose)) {
+    insight::format_alert(
+      "\nNot all rows are shown in the output. Use `print(..., n = Inf)` to show all rows."
+    )
+  }
+}
+
+
+.print_footnote <- function(x) {
+  msg <- NULL
   consv <- attr(x, "constant.values")
-  terms_arg <- attr(x, "terms")
   ci.lvl <- attr(x, "ci.lvl")
-
-  # fix terms for survival models
-  a1 <- attr(x, "fitfun", exact = TRUE)
-  a2 <- attr(x, "y.title", exact = TRUE)
-
-  if (!is.null(a1) && !is.null(a2) && a1 == "coxph" && a2 != "Risk Score" && !"time" %in% terms_arg) {
-    terms_arg <- c("time", terms_arg)
-  }
-
-  # use focal term as column name
-  focal_term <- terms_arg[1]
-  colnames(x)[1] <- focal_term
-
-  x <- .round_numeric(x, digits = digits)
-
-  # justify terms
-  tl <- length(terms_arg)
-  if (tl > 2) {
-    terms_arg[2:tl] <- format(terms_arg[2:tl], justify = "right")
-  }
-
-  # if we have groups, show n rows per group
-  .n <- 1
-
-  # we do not simply count rows, but rather the number of combinations
-  # when we have facets / groups / response.levels. These are separated
-  # with own heading, making the output probably too long. Thus, we
-  # decide on how many rows per "subheading" to be printed also based on
-  # the number of combinations of groups, facets and response level
-
-  if (has_groups) {
-    .n <-  .n_distinct(x$group)
-    if (!is.null(terms_arg) && length(terms_arg) >= 2) {
-      vals <- sprintf("%s = %s", terms_arg[2], as.character(x$group))
-      lvls <- unique(vals)
-      x$group <- factor(vals, levels = lvls)
-    }
-  }
-
-  if (has_facets) {
-    .n <- .n * .n_distinct(x$facet)
-    if (!is.null(terms_arg) && length(terms_arg) >= 3) {
-      x$facet <- sprintf("%s = %s", terms_arg[3], as.character(x$facet))
-    }
-  }
-
-  if (has_panel) {
-    .n <- .n * .n_distinct(x$panel)
-    if (!is.null(terms_arg) && length(terms_arg) >= 4) {
-      x$panel <- sprintf("%s = %s", terms_arg[4], as.character(x$panel))
-    }
-  }
-
-  if (has_response) {
-    .n <- .n * .n_distinct(x$response.level)
-    vals <- sprintf("Response Level = %s", as.character(x$response.level))
-    lvls <- unique(vals)
-    x$response.level <- ordered(vals, levels = lvls)
-  }
-
-  # make sure that by default not too many rows are printed. The larger ".n" is
-  # (i.e. the more subheadings we have, see code above), the fewer rows we want
-  # per subheading. For factors, however, we want to show all levels
-  if (missing(n) && !x_is_factor) {
-    n <- if (.n >= 6) {
-      4
-    } else if (.n >= 4 && .n < 6) {
-      5
-    } else if (.n >= 2 && .n < 4) {
-      6
-    } else {
-      8
-    }
-  } else if (x_is_factor) {
-    n <- Inf
-  }
-
-  if (!has_groups) {
-
-    if (has_response) {
-      x$.nest <- tapply(x$predicted, list(x$response.level), NULL)
-      xx <- split(x, x$.nest)
-      for (i in xx) {
-        insight::print_color(sprintf("\n# %s\n\n", i$response.level[1]), "red")
-        .print_block(i, n, digits, ci.lvl, ...)
-      }
-    } else {
-      cat("\n")
-      if (.obj_has_name(x, "group")) x <- .remove_column(x, "group")
-      .print_block(x, n, digits, ci.lvl, ...)
-    }
-
-  } else if (has_groups && !has_facets) {
-
-    if (has_response) {
-      x$.nest <- tapply(x$predicted, list(x$response.level, x$group), NULL)
-      xx <- split(x, x$.nest)
-      for (i in xx) {
-        insight::print_color(sprintf("\n# %s\n# %s\n\n", i$response.level[1], i$group[1]), "red")
-        .print_block(i, n, digits, ci.lvl, ...)
-      }
-    } else {
-      x$.nest <- tapply(x$predicted, list(x$group), NULL)
-      xx <- split(x, x$.nest)
-      for (i in xx) {
-        insight::print_color(sprintf("\n# %s\n\n", i$group[1]), "red")
-        .print_block(i, n, digits, ci.lvl, ...)
-      }
-    }
-
-  } else if (has_groups && has_facets && !has_panel) {
-
-    if (has_response) {
-      x$.nest <- tapply(x$predicted, list(x$response.level, x$group, x$facet), NULL)
-      xx <- split(x, x$.nest)
-      for (i in xx) {
-        insight::print_color(sprintf("\n# %s\n# %s\n# %s\n\n", i$response.level[1], i$group[1], i$facet[1]), "red")
-        .print_block(i, n, digits, ci.lvl, ...)
-      }
-    } else {
-      x$.nest <- tapply(x$predicted, list(x$group, x$facet), NULL)
-      xx <- split(x, x$.nest)
-      for (i in xx) {
-        insight::print_color(sprintf("\n# %s\n# %s\n\n", i$group[1], i$facet[1]), "red")
-        .print_block(i, n, digits, ci.lvl, ...)
-      }
-    }
-
-  } else if (has_response) {
-    x$.nest <- tapply(x$predicted, list(x$response.level, x$group, x$facet, x$panel), NULL)
-    xx <- split(x, x$.nest)
-    for (i in xx) {
-      insight::print_color(sprintf(
-        "\n# %s\n# %s\n# %s\n# %s\n\n",
-        i$response.level[1],
-        i$group[1],
-        i$facet[1],
-        i$panel[1]
-      ), "red")
-      .print_block(i, n, digits, ci.lvl, ...)
-    }
-  } else {
-    x$.nest <- tapply(x$predicted, list(x$group, x$facet, x$panel), NULL)
-    xx <- split(x, x$.nest)
-    for (i in xx) {
-      insight::print_color(sprintf("\n# %s\n# %s\n# %s\n\n", i$group[1], i$facet[1], i$panel[1]), "red")
-      .print_block(i, n, digits, ci.lvl, ...)
-    }
-  }
 
   cv <- lapply(consv, function(.x) {
     if (is.numeric(.x)) {
@@ -212,37 +114,12 @@ print.ggeffects <- function(x, n = 10, digits = 2, value_labels = FALSE, verbose
       cv.space2 <- max(nchar(mcv))
     }
 
-    insight::print_color(paste0(
+    msg <- c(paste0(
       "\nAdjusted for:\n",
       paste0(sprintf("* %*s = %*s", cv.space, cv.names, cv.space2, cv), collapse = "\n")
-    ), "blue")
-
-    cat("\n")
+    ), "yellow")
   }
-
-  # show msg?
-  if (missing(verbose)) {
-    verbose <- isTRUE(attr(x, "verbose", exact = TRUE))
-  }
-
-  fitfun <- attr(x, "fitfun", exact = TRUE)
-  if (has_se && !is.null(fitfun) && fitfun != "lm" && isTRUE(verbose)) {
-    insight::format_alert("\nStandard errors are on the link-scale (untransformed).")
-  }
-
-  predint <- attr(x, "prediction.interval", exact = TRUE)
-  if (!is.null(predint) && isTRUE(predint) && isTRUE(verbose)) {
-    insight::format_alert(
-      "\nIntervals are prediction intervals. Use `interval = \"confidence\"` to return regular confidence intervals."
-    )
-  }
-
-  # tell user about truncated output
-  if ((.n * n) < nrow(x) && isTRUE(verbose)) {
-    insight::format_alert(
-      "\nNot all rows are shown in the output. Use `print(..., n = Inf)` to show all rows."
-    )
-  }
+  msg
 }
 
 
@@ -260,6 +137,28 @@ print_html.ggeffects <- function(x, ...) {
     ...
   )
 }
+
+
+# helper --------------------
+
+.get_sample_rows <- function(x, n) {
+  nr.of.rows <- seq_len(nrow(x))
+
+  if (n < length(nr.of.rows)) {
+    sample.rows <- round(c(
+      min(nr.of.rows),
+      stats::quantile(nr.of.rows, seq_len(n - 2) / n),
+      max(nr.of.rows)
+    ))
+  } else {
+    sample.rows <- nr.of.rows
+  }
+
+  sample.rows
+}
+
+
+
 
 ## printing with tinytables
 
@@ -295,51 +194,3 @@ print_html.ggeffects <- function(x, ...) {
 
 # tt(x) |>
 #   group_tt(i = row_header_labels)
-
-
-
-# helper --------------------
-
-
-.get_sample_rows <- function(x, n) {
-  nr.of.rows <- seq_len(nrow(x))
-
-  if (n < length(nr.of.rows)) {
-    sample.rows <- round(c(
-      min(nr.of.rows),
-      stats::quantile(nr.of.rows, seq_len(n - 2) / n),
-      max(nr.of.rows)
-    ))
-  } else {
-    sample.rows <- nr.of.rows
-  }
-
-  sample.rows
-}
-
-
-.print_block <- function(i, n, digits, ci.lvl, ...) {
-  i <- as.data.frame(i)
-  i <- i[setdiff(colnames(i), c("group", "facet", "panel", "response.level", ".nest"))]
-  dd <- i[.get_sample_rows(i, n), , drop = FALSE]
-
-  if ("conf.low" %in% colnames(dd) && "conf.high" %in% colnames(dd)) {
-    dd$CI <- insight::format_ci(dd$conf.low, dd$conf.high, digits = digits, width = "auto")
-    dd$CI <- gsub("95% CI ", "", dd$CI, fixed = TRUE)
-
-    if (is.null(ci.lvl)) {
-      ci.lvl <- 0.95
-    }
-    colnames(dd)[which(colnames(dd) == "CI")] <- sprintf("%g%% CI", 100 * ci.lvl)
-
-    dd$conf.low <- NULL
-    dd$conf.high <- NULL
-  }
-
-  if ("std.error" %in% colnames(dd)) {
-    colnames(dd)[which(colnames(dd) == "std.error")] <- "SE"
-  }
-
-  colnames(dd)[which(colnames(dd) == "predicted")] <- "Predicted"
-  cat(insight::export_table(dd, digits = digits, protect_integers = TRUE))
-}
