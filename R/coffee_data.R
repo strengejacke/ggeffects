df935028e9280427f2abf5c553ebc61eb23425f2
Daniel <mail@danielluedecke.de>
2024-04-09 12:08:21 +0200

    doc data

---
+++ R/coffee_data.R
@@ -0,0 +1,11 @@
+#' @docType data
+#' @title Sample dataset
+#' @name coffee_data
+#' @keywords data
+#'
+#' @description A sample data set.
+#'
+#' @examples
+#' # Attach EFC-data
+#' data(coffee_data)
+NULL
