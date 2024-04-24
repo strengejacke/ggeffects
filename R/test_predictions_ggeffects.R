901cfb971eb18019bd73b2dea8bc9b87766e92ba
Daniel <mail@danielluedecke.de>
2024-04-24 17:55:35 +0200

    fix

--- R/test_predictions_ggeffects.R
+++ R/test_predictions_ggeffects.R
@@ -47,7 +47,7 @@
 
   ## TODO: include vcov(predictions) in calculation of standard errors?
   # vcov matrix, for adjusting se
-  # vcov_matrix <- .safe(stats::vcov(model, verbose = FALSE, ...))
+  vcov_matrix <- .safe(stats::vcov(model, verbose = FALSE, ...))
 
   # we now need to get the model object
   object <- .get_model_object(object)
