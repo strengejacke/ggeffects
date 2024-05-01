abd77b237e3eee2d483d1aea30719a146c98f302
Daniel <mail@danielluedecke.de>
2024-05-01 22:00:47 +0200

    fix

--- tests/testthat/test-splines.R
+++ tests/testthat/test-splines.R
@@ -8,7 +8,7 @@ withr::with_environment(
     # custom function for splines (#260)
     fit <- lm(barthtot ~ c12hour + splines::bs(neg_c_7, df = 9) * c161sex + e42dep, data = efc)
     p1 <- ggpredict(fit, terms = c("neg_c_7", "c161sex", "e42dep"))
-    bs9 <- function(x) splines::bs(x, df = 9) # <---- put spline in a custom function
+    bs9 <<- function(x) splines::bs(x, df = 9) # <---- put spline in a custom function
     fit <- lm(barthtot ~ c12hour + bs9(neg_c_7) * c161sex + e42dep, data = efc)
     p2 <- ggpredict(fit, terms = c("neg_c_7", "c161sex", "e42dep"))
     expect_equal(p1$predicted, p2$predicted, tolerance = 1e-4)
