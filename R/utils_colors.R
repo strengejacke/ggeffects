a1643a071d6900d429c6621af6939a8545703303
Daniel <mail@danielluedecke.de>
2024-10-23 18:45:53 +0200

    Add options to return color palettes

--- R/utils_colors.R
+++ R/utils_colors.R
@@ -5,11 +5,11 @@
     geom.colors <- tolower(geom.colors)
     # check for color brewer palette
     if (geom.colors[1] %in% names(ggeffects_colors)) {
-      geom.colors <- ggeffects_pal(palette = geom.colors[1], n = collen)
+      geom.colors <- ggeffects_palette(palette = geom.colors[1], n = collen)
     } else if (geom.colors[1] == "bw") {
       geom.colors <- rep("black", times = collen)
     } else if (geom.colors[1] == "gs") {
-      geom.colors <- ggeffects_pal(palette = "greyscale", n = collen)
+      geom.colors <- ggeffects_palette(palette = "greyscale", n = collen)
       # do we have correct amount of colours?
     } else if (length(geom.colors) > 1 && continuous) {
       # preserve colors as is for latter use in gradient scale
@@ -21,11 +21,11 @@
       # warn user abount wrong color palette
       insight::format_alert(sprintf("Insufficient length of color palette provided. %i color values needed.", collen))
       # set default palette
-      geom.colors <- ggeffects_pal(palette = "set1", n = collen)
+      geom.colors <- ggeffects_palette(palette = "set1", n = collen)
     }
   } else {
     if (collen > 1) {
-      geom.colors <- ggeffects_pal(palette = "set1", n = collen)
+      geom.colors <- ggeffects_palette(palette = "set1", n = collen)
     } else {
       geom.colors <- "black"
     }
