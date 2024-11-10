8e4f758dbf349ae061ab9e543f23ccd43886b19f
Daniel <mail@danielluedecke.de>
2024-11-10 14:36:50 +0100

    add test

---
+++ tests/testthat/test-brglm.R
@@ -0,0 +1,15 @@
+skip_on_os(c("mac", "linux", "solaris"))
+skip_if_not_installed("brglm")
+
+test_that(" ggpredict brglm", {
+  data(lizards, package = "brglm")
+  m <- brglm::brglm(
+    cbind(grahami, opalinus) ~ height + diameter + light + time,
+    family = binomial(logit),
+    data = lizards,
+    method = "brglm.fit"
+  )
+  out <- predict_response(m, "height")
+  expect_equal(out$predicted, c(0.8701, 0.95295), tolerance = 1e-4)
+  expect_equal(out$conf.low, c(0.77567, 0.90524), tolerance = 1e-4)
+})
