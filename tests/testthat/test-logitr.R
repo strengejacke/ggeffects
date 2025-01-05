47de31305a131e045f7d461272fe734f69583d6a
Daniel <mail@danielluedecke.de>
2025-01-05 12:12:01 +0100

    fix

--- tests/testthat/test-logitr.R
+++ tests/testthat/test-logitr.R
@@ -12,7 +12,7 @@ test_that("ggpredict, logistr", {
     pars    = c("price", "feat", "brand")
   )
   pr <- predict_response(m, "brand")
-  expect_equal(pr$predicted[1], c(0.27506, 0.0067, 0.14487, 0.57337), tolerance = 1e-3)
+  expect_equal(pr$predicted, c(0.27506, 0.0067, 0.14487, 0.57337), tolerance = 1e-3)
   expect_equal(pr$conf.low, c(0.24978, 0.00487, 0.12894, 0.53434), tolerance = 1e-3)
 
   expect_error(ggemmeans(m, "brand"))
