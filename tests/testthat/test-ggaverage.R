7941fa6cbce853d5fdffb1f37553ec2868d594e1
Daniel <mail@danielluedecke.de>
2025-02-28 20:15:01 +0100

    update tests

--- tests/testthat/test-ggaverage.R
+++ tests/testthat/test-ggaverage.R
@@ -21,11 +21,6 @@ test_that("ggaverage works with condition", {
   expect_equal(out1$conf.low, c(4.35203, 3.19893, 3.48993), tolerance = 1e-4)
   expect_equal(out1$conf.low, out3$asymp.LCL, tolerance = 1e-4)
 
-  # test predictions
-  out <- test_predictions(moff, c("x2", "offset_1 = 1"))
-  expect_equal(out$Difference[c(1, 3)], diff(out1$predicted), tolerance = 1e-4)
-  expect_equal(out$p, c(0.3571, 0.42296, 0.76791), tolerance = 1e-4)
-
   d <- data_grid(moff, "x2", condition = c(offset_1 = 2))
   out1 <- ggaverage(moff, "x2", condition = c(offset_1 = 2))
   out2 <- marginaleffects::avg_predictions(moff, variables = lapply(d, unique))
