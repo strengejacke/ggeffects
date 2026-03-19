8286eef8066db8a7b2cc90a589bc6a1bf28b6121
alix-data <alix.hrtr@gmail.com>
2026-03-19 12:00:37 +0100

    methode_fonctionnelle_codee_par_chatgpt

--- R/get_predictions_rma.R
+++ R/get_predictions_rma.R
@@ -22,7 +22,7 @@ data_effets <- escalc(
 modele_rma <- rma(yi, vi, mods = ~ alloc, data = data_effets)
 
 # ce qui devrait marcher
-ggeffects::ggpredict(modele_rma , terms = "alloc")
+#ggeffects::ggpredict(modele_rma , terms = "alloc")
 
 # sauf qu'on a l'erreur : 
 
@@ -30,4 +30,79 @@ ggeffects::ggpredict(modele_rma , terms = "alloc")
 #replacement has 13 rows, data has 3
 
 
+reg1 <- lm(Sepal.Length ~ Sepal.Width+Species,data=iris)
+pre <- ggpredict(reg1)
+
+
+
+
+library(metafor)
+library(ggeffects)
+
+get_predictions.rma <- function(model,
+                                data_grid,
+                                terms = NULL,
+                                ci_level = 0.95,
+                                model_info = NULL,
+                                type = NULL,
+                                typical = NULL,
+                                vcov = NULL,
+                                vcov_args = NULL,
+                                condition = NULL,
+                                interval = NULL,
+                                link_inverse = NULL,
+                                bias_correction = FALSE,
+                                verbose = TRUE,
+                                ...) {
+  
+  # On récupère la formule des modérateurs du modèle rma
+  mods_formula <- eval(model$call$mods)
+  
+  # On construit la matrice du modèle SEULEMENT avec ces modérateurs
+  X_new <- model.matrix(mods_formula, data = data_grid)
+  
+  # predict.rma() attend newmods sans intercept
+  if ("(Intercept)" %in% colnames(X_new)) {
+    newmods <- X_new[, colnames(X_new) != "(Intercept)", drop = FALSE]
+  } else {
+    newmods <- X_new
+  }
+  
+  if (ncol(newmods) == 0) {
+    newmods <- NULL
+  }
+  
+  pr <- predict(
+    model,
+    newmods = newmods,
+    level = ci_level * 100,
+    ...
+  )
+  
+  data_grid$predicted <- as.numeric(pr$pred)
+  data_grid$conf.low  <- as.numeric(pr$ci.lb)
+  data_grid$conf.high <- as.numeric(pr$ci.ub)
+  
+  data_grid
+}
+
+registerS3method("get_predictions", "rma", get_predictions.rma,
+                 envir = asNamespace("ggeffects"))
+registerS3method("get_predictions", "rma.uni", get_predictions.rma,
+                 envir = asNamespace("ggeffects"))
+
+# --- ton exemple ---
+data <- dat.bcg
+data$alloc <- factor(data$alloc)
+
+data_effets <- escalc(
+  measure = "RR",
+  ai = tpos, bi = tneg,
+  ci = cpos, di = cneg,
+  data = data
+)
+
+modele_rma <- rma(yi, vi, mods = ~ alloc, data = data_effets)
+
+ggeffects::ggpredict(modele_rma, terms = "alloc")
 
