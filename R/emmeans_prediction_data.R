505567ffed7209477d050024776970aae9cbc239
Daniel <mail@danielluedecke.de>
2026-06-05 16:27:11 +0200

    Bug: ggemmeans throws "terms not found" for clmm models due to environment scoping failure in .check_vars() Fixes #676

--- R/emmeans_prediction_data.R
+++ R/emmeans_prediction_data.R
@@ -1,11 +1,13 @@
-.emmeans_mixed_zi <- function(model,
-                              data_grid,
-                              cleaned_terms,
-                              ci_level = NULL,
-                              bias_correction = FALSE,
-                              residual_variance = NULL,
-                              weights = NULL,
-                              ...) {
+.emmeans_mixed_zi <- function(
+  model,
+  data_grid,
+  cleaned_terms,
+  ci_level = NULL,
+  bias_correction = FALSE,
+  residual_variance = NULL,
+  weights = NULL,
+  ...
+) {
   if (inherits(model, "glmmTMB")) {
     .ggemmeans_glmmTMB(
       model,
@@ -32,57 +34,99 @@
 }
 
 
-.emmeans_prediction_data <- function(model,
-                                     data_grid,
-                                     cleaned_terms,
-                                     ci_level,
-                                     pmode,
-                                     model_info,
-                                     interval = NULL,
-                                     model_data = NULL,
-                                     vcov_info = NULL,
-                                     bias_correction = FALSE,
-                                     residual_variance = NULL,
-                                     weights = NULL,
-                                     verbose = TRUE,
-                                     ...) {
+.emmeans_prediction_data <- function(
+  model,
+  data_grid,
+  cleaned_terms,
+  ci_level,
+  pmode,
+  model_info,
+  interval = NULL,
+  model_data = NULL,
+  vcov_info = NULL,
+  bias_correction = FALSE,
+  residual_variance = NULL,
+  weights = NULL,
+  verbose = TRUE,
+  ...
+) {
   if (inherits(model, "MCMCglmm")) {
     .ggemmeans_predict_MCMCglmm(
-      model, data_grid, cleaned_terms, ci_level, pmode,
-      interval = interval, model_data = model_data, weights = weights, ...
+      model,
+      data_grid,
+      cleaned_terms,
+      ci_level,
+      pmode,
+      interval = interval,
+      model_data = model_data,
+      weights = weights,
+      ...
     )
-  } else if (!is.null(model_info) && (model_info$is_ordinal || model_info$is_multinomial || model_info$is_categorical)) { # nolint
+  } else if (
+    !is.null(model_info) &&
+      (model_info$is_ordinal ||
+        model_info$is_multinomial ||
+        model_info$is_categorical)
+  ) {
+    # nolint
     .ggemmeans_predict_ordinal(
-      model, data_grid, cleaned_terms, ci_level,
-      interval = interval, model_data = model_data, weights = weights, ...
+      model,
+      data_grid,
+      cleaned_terms,
+      ci_level,
+      pmode,
+      interval = interval,
+      model_data = model_data,
+      weights = weights,
+      ...
     )
   } else if (inherits(model, c("gls", "lme"))) {
     .ggemmeans_predict_nlme(
-      model, data_grid, cleaned_terms, ci_level,
-      interval = interval, model_data = model_data,
+      model,
+      data_grid,
+      cleaned_terms,
+      ci_level,
+      interval = interval,
+      model_data = model_data,
       bias_correction = bias_correction,
-      residual_variance = residual_variance, weights = weights, ...
+      residual_variance = residual_variance,
+      weights = weights,
+      ...
     )
   } else {
     .ggemmeans_predict_generic(
-      model, data_grid, cleaned_terms, ci_level, pmode,
-      interval = interval, model_data = model_data, vcov_info = vcov_info,
-      verbose = verbose, bias_correction = bias_correction,
-      residual_variance = residual_variance, weights = weights, ...
+      model,
+      data_grid,
+      cleaned_terms,
+      ci_level,
+      pmode,
+      interval = interval,
+      model_data = model_data,
+      vcov_info = vcov_info,
+      verbose = verbose,
+      bias_correction = bias_correction,
+      residual_variance = residual_variance,
+      weights = weights,
+      ...
     )
   }
 }
 
 
-.ggemmeans_MixMod <- function(model,
-                              data_grid,
-                              cleaned_terms,
-                              ci_level = NULL,
-                              bias_correction = FALSE,
-                              residual_variance = NULL,
-                              weights = NULL,
-                              ...) {
-  insight::check_if_installed("emmeans", "to compute estimated marginal means for MixMod-models")
+.ggemmeans_MixMod <- function(
+  model,
+  data_grid,
+  cleaned_terms,
+  ci_level = NULL,
+  bias_correction = FALSE,
+  residual_variance = NULL,
+  weights = NULL,
+  ...
+) {
+  insight::check_if_installed(
+    "emmeans",
+    "to compute estimated marginal means for MixMod-models"
+  )
 
   # get additional arguments
   dot_args <- list(...)
@@ -100,7 +144,10 @@
     weights = weights
   )
 
-  x1 <- as.data.frame(suppressWarnings(do.call(emmeans::emmeans, c(arg_list, dot_args))))
+  x1 <- as.data.frame(suppressWarnings(do.call(
+    emmeans::emmeans,
+    c(arg_list, dot_args)
+  )))
 
   arg_list <- list(
     model,
@@ -111,21 +158,29 @@
     bias.adjust = bias_correction
   )
 
-  x2 <- as.data.frame(suppressWarnings(do.call(emmeans::emmeans, c(arg_list, dot_args))))
+  x2 <- as.data.frame(suppressWarnings(do.call(
+    emmeans::emmeans,
+    c(arg_list, dot_args)
+  )))
 
   list(x1 = x1, x2 = x2)
 }
 
 
-.ggemmeans_glmmTMB <- function(model,
-                               data_grid,
-                               cleaned_terms,
-                               ci_level = NULL,
-                               bias_correction = FALSE,
-                               residual_variance = NULL,
-                               weights = NULL,
-                               ...) {
-  insight::check_if_installed("emmeans", "to compute estimated marginal means for glmmTMB-models")
+.ggemmeans_glmmTMB <- function(
+  model,
+  data_grid,
+  cleaned_terms,
+  ci_level = NULL,
+  bias_correction = FALSE,
+  residual_variance = NULL,
+  weights = NULL,
+  ...
+) {
+  insight::check_if_installed(
+    "emmeans",
+    "to compute estimated marginal means for glmmTMB-models"
+  )
   # all model variables
   vars <- insight::find_variables(model)
   # match variables for conditional model
@@ -150,7 +205,10 @@
     weights = weights
   )
 
-  x1 <- as.data.frame(suppressWarnings(do.call(emmeans::emmeans, c(arg_list, dot_args))))
+  x1 <- as.data.frame(suppressWarnings(do.call(
+    emmeans::emmeans,
+    c(arg_list, dot_args)
+  )))
 
   arg_list <- list(
     model,
@@ -161,42 +219,55 @@
     bias.adjust = bias_correction
   )
 
-  x2 <- as.data.frame(suppressWarnings(do.call(emmeans::emmeans, c(arg_list, dot_args))))
+  x2 <- as.data.frame(suppressWarnings(do.call(
+    emmeans::emmeans,
+    c(arg_list, dot_args)
+  )))
 
   list(x1 = x1, x2 = x2)
 }
 
 
-.ggemmeans_predict_ordinal <- function(model,
-                                       data_grid,
-                                       cleaned_terms,
-                                       ci_level,
-                                       interval = NULL,
-                                       model_data = NULL,
-                                       weights = NULL,
-                                       ...) {
-  tmp <- suppressMessages(emmeans::emmeans(
+.ggemmeans_predict_ordinal <- function(
+  model,
+  data_grid,
+  cleaned_terms,
+  ci_level,
+  pmode,
+  interval = NULL,
+  model_data = NULL,
+  weights = NULL,
+  ...
+) {
+  dots <- list(...)
+  # setup arguments
+  emmeans_args <- list(
     model,
     specs = c(insight::find_response(model, combine = FALSE), cleaned_terms),
     at = data_grid,
-    mode = "prob",
-    weights = weights,
-    ...
-  ))
+    mode = pmode,
+    weights = weights
+  )
 
-  .ggemmeans_add_confint(model, tmp, ci_level, pmode = "prob", interval)
+  dots[names(emmeans_args)] <- NULL
+  emmeans_args <- insight::compact_list(c(emmeans_args, dots))
+  tmp <- suppressMessages(do.call(emmeans::emmeans, emmeans_args))
+
+  .ggemmeans_add_confint(model, tmp, ci_level, pmode = pmode, interval)
 }
 
 
-.ggemmeans_predict_MCMCglmm <- function(model,
-                                        data_grid,
-                                        cleaned_terms,
-                                        ci_level,
-                                        pmode,
-                                        interval = NULL,
-                                        model_data = NULL,
-                                        weights = NULL,
-                                        ...) {
+.ggemmeans_predict_MCMCglmm <- function(
+  model,
+  data_grid,
+  cleaned_terms,
+  ci_level,
+  pmode,
+  interval = NULL,
+  model_data = NULL,
+  weights = NULL,
+  ...
+) {
   tmp <- emmeans::emmeans(
     model,
     specs = cleaned_terms,
@@ -211,19 +282,21 @@
 }
 
 
-.ggemmeans_predict_generic <- function(model,
-                                       data_grid,
-                                       cleaned_terms,
-                                       ci_level,
-                                       pmode,
-                                       interval = NULL,
-                                       model_data = NULL,
-                                       vcov_info = NULL,
-                                       bias_correction = FALSE,
-                                       residual_variance = NULL,
-                                       weights = NULL,
-                                       verbose = TRUE,
-                                       ...) {
+.ggemmeans_predict_generic <- function(
+  model,
+  data_grid,
+  cleaned_terms,
+  ci_level,
+  pmode,
+  interval = NULL,
+  model_data = NULL,
+  vcov_info = NULL,
+  bias_correction = FALSE,
+  residual_variance = NULL,
+  weights = NULL,
+  verbose = TRUE,
+  ...
+) {
   # get additional arguments
   dot_args <- list(...)
   # modify sigma, if necessary
@@ -254,12 +327,19 @@
   # if data could not be recovered, tmp is NULL. Try again, this time
   # providing the data directly
   if (is.null(tmp)) {
-    emmeans_args$data <- insight::get_data(model, source = "frame", verbose = FALSE)
+    emmeans_args$data <- insight::get_data(
+      model,
+      source = "frame",
+      verbose = FALSE
+    )
     tmp <- tryCatch(
       suppressWarnings(do.call(emmeans::emmeans, emmeans_args)),
       error = function(e) {
         if (verbose) {
-          insight::print_color("Can't compute estimated marginal means, 'emmeans::emmeans()' returned an error.\n\n", "red") # nolint
+          insight::print_color(
+            "Can't compute estimated marginal means, 'emmeans::emmeans()' returned an error.\n\n",
+            "red"
+          ) # nolint
           cat(sprintf("Reason: %s\n", e$message))
           cat("You may try 'ggpredict()' or 'ggeffect()'.\n\n")
         }
@@ -277,16 +357,18 @@
 }
 
 
-.ggemmeans_predict_nlme <- function(model,
-                                    data_grid,
-                                    cleaned_terms,
-                                    ci_level,
-                                    interval = NULL,
-                                    model_data = NULL,
-                                    bias_correction = FALSE,
-                                    residual_variance = NULL,
-                                    weights = NULL,
-                                    ...) {
+.ggemmeans_predict_nlme <- function(
+  model,
+  data_grid,
+  cleaned_terms,
+  ci_level,
+  interval = NULL,
+  model_data = NULL,
+  bias_correction = FALSE,
+  residual_variance = NULL,
+  weights = NULL,
+  ...
+) {
   # get additional arguments
   dot_args <- list(...)
   # modify sigma, if necessary
@@ -306,7 +388,10 @@
   tmp <- tryCatch(
     suppressWarnings(do.call(emmeans::emmeans, c(emmeans_args, dot_args))),
     error = function(e) {
-      insight::print_color("Can't compute estimated marginal means, 'emmeans::emmeans()' returned an error.\n\n", "red")
+      insight::print_color(
+        "Can't compute estimated marginal means, 'emmeans::emmeans()' returned an error.\n\n",
+        "red"
+      )
       cat(sprintf("Reason: %s\n", e$message))
       cat("You may try 'ggpredict()' or 'ggeffect()'.\n\n")
       NULL
